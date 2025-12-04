5b4d70b2f7d50199f2f4c2daedaa17a1dfed3f34
Stefan Drexler <stefan.drexler@hm.edu>
2025-12-04 22:26:46 +0100

    test

--- src/main/java/demo/WriteObjectToFile.java
+++ src/main/java/demo/WriteObjectToFile.java
@@ -4,10 +4,10 @@ import java.io.*;
 
 public class WriteObjectToFile {
     public static void main(String[] args) throws Exception {
-        FalseMessage msg = new FalseMessage("Hallo vom Java‑Objekt!");
+        Message msg = new Message("Hallo vom Java‑Objekt!");
 
         try (ObjectOutputStream oos =
-                     new ObjectOutputStream(new FileOutputStream("false_msg.bin"))) {
+                     new ObjectOutputStream(new FileOutputStream("msg.bin"))) {
             oos.writeObject(msg);
         }
 
